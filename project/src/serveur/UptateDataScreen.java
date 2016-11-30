53b5c5e74cac973a8fa1caa3e9f5e58107d6278c
Francois <francois.georis@student.unamur.be>
2016-11-30 13:43:51 +0100

    Ajout page 2 (cercle)

--- project/src/serveur/UptateDataScreen.java
+++ project/src/serveur/UptateDataScreen.java
@@ -19,6 +19,9 @@ public class UptateDataScreen implements Runnable {
     private File f;
     private File f2;
     private Connection connection;
+    private String page;
+    private  int numBoite;
+    String page2;
 
 
 
@@ -26,9 +29,12 @@ public class UptateDataScreen implements Runnable {
     public UptateDataScreen (Connection c,int numBoite){
         this.state =state;
         ResourceBundle rb = ResourceBundle.getBundle("serveur.domaine.properties.config");
-        //String nf1 = rb.getString("nf1");
-        String nf2 = rb.getString("nf2");
-       // f = new File(nf1) ;
+        String nf1 = rb.getString("nf1");
+        String nf2 = rb.getString("nf2")+numBoite+".csv";
+        f = new File(nf1) ;
+        page=rb.getString("page1");
+        page2=rb.getString("page2");
+        this.numBoite=numBoite;
 
 
 
@@ -50,36 +56,35 @@ public class UptateDataScreen implements Runnable {
 
 
         try {
-            Process proc = Runtime.getRuntime().exec("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe http://localhost/mapage3.html");
+            Process proc = Runtime.getRuntime().exec(page);
+            Process proc2 = Runtime.getRuntime().exec(page2);
 
             while (true) {
 
 
-              /*  ResultSet rs = state.executeQuery("SELECT * FROM Box1;");
+                ResultSet rs = state.executeQuery("SELECT Ind ,COUNT(Valeur) AS nb FROM Boite1 GROUP BY Ind;");
 
                 FileWriter fw = new FileWriter(f);
-                int d;
-                int valeur;
-                fw.write("date\tvaleur\n");
+
+                fw.write("age,population\n");
 
 
 
 
                 while (rs.next()) {
-                    d = rs.getInt("Date");
-                    valeur = rs.getInt("Valeur");
-                    date = new Date(d);
 
 
-                    fw.write(date.getHours() + "\t" + valeur + "\r\n");
+
+
+                    fw.write("Boutton "+rs.getInt("Ind") + "," + rs.getInt("nb") + "\r\n");
                 }
 
 
                 rs.close();
-                fw.close();*/
+                fw.close();
 
                 //Structure données mapage3
-                ResultSet rs3 = state.executeQuery("SELECT  Ind FROM Boite1 GROUP BY Ind ORDER BY Ind ASC;");
+                ResultSet rs3 = state.executeQuery("SELECT  Ind FROM Boite"+numBoite+" GROUP BY Ind ORDER BY Ind ASC;");
                 int tabBoutton[]=new int[8];
                 int lgTab=0;
                 FileWriter fw2 = new FileWriter(f2);
@@ -100,7 +105,7 @@ public class UptateDataScreen implements Runnable {
                 int jour;
                 int derniereJour=-1;
 
-                ResultSet rs2 = state.executeQuery("SELECT Ind,Jour,Heure,COUNT(Valeur) AS nb  FROM Boite1 GROUP BY Ind,Jour,Heure ORDER BY  Jour ASC,Heure ASC;");
+                ResultSet rs2 = state.executeQuery("SELECT Ind,Jour,Heure,COUNT(Valeur) AS nb  FROM Boite"+numBoite+" GROUP BY Ind,Jour,Heure ORDER BY  Jour ASC,Heure ASC;");
                 while (rs2.next()) {
                     heure = rs2.getInt("Heure");
                     jour = rs2.getInt("Jour");
